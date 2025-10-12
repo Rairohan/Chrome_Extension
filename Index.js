54de550939b117cd120d436c11a90c8848dce97b
rohan <rairohan14303@gmail.com>
2025-10-12 11:29:29 +0545

    new Update

--- Index.js
+++ Index.js
@@ -1,4 +1,13 @@
+let myLeads= []
+const inputEl = document.getElementById("input-el")//const used as it is has final vlaue which doesn't changes
+const ulEl = document.getElementById("ul")
+
 let InputBtn = document.getElementById("input-btn")
 InputBtn.addEventListener("click",function(){
-    console.log("hey!")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+
+for(i=0;i<myLeads.length;i++){
+   ulEl.innerHTML += "<li>"+myLeads[i]+"</li>"//innerHtml simply grabs the element and changes both element tag and the content
+}
 })
