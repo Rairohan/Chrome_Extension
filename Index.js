5c79d397b1569de2ab61fe824aece25eb2954e72
rohan <rairohan14303@gmail.com>
2025-10-14 14:49:22 +0545

    link modified

--- Index.js
+++ Index.js
@@ -8,17 +8,17 @@ InputBtn.addEventListener("click",function(){
     const newLead= inputEl.value
     myLeads.push(newLead)
     console.log(myLeads)
-    inputEl.value=""//we are changing the value inside input-el id tag not the reference
     renderLeads()
+     inputEl.value=""//we are changing the value inside input-el id tag not the reference
 })
 function renderLeads(){
 let Listitem = ""
 for(let i=0;i<myLeads.length;i++){
    
-//    Listitem += "<li> <a href='"+ myLeads[i]+"' target = 'blank'>"+myLeads[i]+"</a></li>"
+ Listitem += // "<li> <a href='"+ myLeads[i]+"' target = 'blank'>"+myLeads[i]+"</a></li>"
       `<li>
-       <a href='${ myLeads[i]}' target = 'blank'>${myLeads[i]}</a>
-       </li>`
+        <a href='${ myLeads[i]}' target = 'blank'>${myLeads[i]}</a>
+        </li>`
        //this method is an alternative (a template string) to the above commented line 18 method
        //  where we use ` to elimate the confusion of "" and '' and it also appears more like html
 }
